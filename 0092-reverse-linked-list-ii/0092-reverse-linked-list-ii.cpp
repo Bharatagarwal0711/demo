994beadd92cb1ee0edc31eb8a20d8ce06a8ade55
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-10 00:57:38 +0530

    Time: 0 ms (100%), Space: 11.3 MB (39.08%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* curr = head, *prev = NULL, *nxt = head->next;
+
+        while(nxt){
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+            nxt = nxt->next;
+        }
+        curr->next = prev;
+        return curr;
+    }
+
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+
+        if(left == right) return head;
+
+        ListNode* a = NULL, *b = NULL, *c = NULL, *d = NULL;
+
+        int count = 0;
+
+        ListNode* temp = head;
+
+        while(temp){
+            count++;
+            if(count == left-1) a = temp;
+            if(left == count) b=temp;
+            if(right == count) c = temp;
+            if(count == right+1) d = temp;
+
+            temp = temp->next;
+        }
+        
+        if(a) a->next = NULL;
+        c->next = NULL;
+        c = reverseList(b);
+        if(a) a->next = c;
+        else head = c;
+        b->next = d;
+
+        return head;
+
+    }
+};
