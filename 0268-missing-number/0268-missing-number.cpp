17bed0fa779201f4455ca96096ac7850273b1d8e
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-10 23:34:00 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        int idx = 0;
-        while(idx<n){
-            int correctIdx = nums[idx];
-            if(nums[idx]>=n || idx==correctIdx) idx++;
-            else swap(nums[idx],nums[correctIdx]);
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]!=i) return i;
-        }
-        return n;
+        int sum = 0;
+        for(int ele: nums) sum += ele;
+
+        return ((n*(n+1))/2 ) - sum;
     }
 };
