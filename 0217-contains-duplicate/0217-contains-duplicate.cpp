8d0b63d5532ceaa91b53fe446c294799f14e7a9d
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-10 22:59:58 +0530

    Time: 98 ms (10.59%), Space: 113.4 MB (5.16%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,7 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
 
         unordered_set<int> st(nums.begin(),nums.end());
-        return nums.size()>st.size();
+        return nums.size()!=st.size();
         
     }
 };
