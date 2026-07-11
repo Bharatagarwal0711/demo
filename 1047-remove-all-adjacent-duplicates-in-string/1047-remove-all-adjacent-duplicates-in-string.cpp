90ba50aed2751b78c952e48766c4fa3e510847b3
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-12 03:11:33 +0530

    Time: 3 ms (81.05%), Space: 13.7 MB (60.64%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        for(char ch: s){
+
+            if(ans.empty() || ans.back()!=ch) ans.push_back(ch);
+            else ans.pop_back();
+            
+        }
+        return ans;
+    }
+};
