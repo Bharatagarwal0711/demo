2fd401636382ce63b1b606bb6e4334a4c6b9fff6
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-12 11:55:20 +0530

    Time: 0 ms (100%), Space: 11.2 MB (74.87%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    int matrixScore(vector<vector<int>>& arr) {
-        int row = arr.size();
-        int col = arr[0].size();
-        for(int i=0;i<row;i++){
-            if(arr[i][0] == 0){
-                for(int j=0;j<col;j++){
-                    if(arr[i][j] == 0) arr[i][j]=1;
-                    else arr[i][j] = 0;
-                }
+    int matrixScore(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        //row check
+
+        for(int i=0;i<rows;i++){
+            if(grid[i][0] == 0){
+                for(int j=0;j<cols;j++)  grid[i][j] = grid[i][j] == 0 ? 1 : 0; 
             }
         }
 
-        for(int j=0;j<col;j++){
-            int noz = 0;
-            int noo = 0;
-            for(int i=0;i<row;i++){
-                if(arr[i][j]==0) noz++;
-                else noo++;
-            }
+        // column check
 
+        for(int j=0;j<cols;j++){
+            int noo = 0;
+            int noz = 0;
+
+            for(int i=0;i<rows;i++){
+                grid[i][j] == 0 ? noz++ : noo++;
+            }
             if(noz>noo){
-                for(int i=0;i<row;i++){
-                    if(arr[i][j] == 0) arr[i][j]=1;
-                    else arr[i][j] = 0;
-                }
+                for(int i=0;i<rows;i++)  grid[i][j] = grid[i][j] == 0 ? 1 : 0;   
             }
         }
 
         int ans = 0;
-        for(int i=0;i<row;i++){
+
+        for(int i=0;i<rows;i++){
             int fact = 1;
-            for(int j=col-1;j>=0;j--){
-                ans += arr[i][j]*fact;
+            for(int j=cols-1;j>=0;j--){
+                ans += grid[i][j]*fact;
                 fact *= 2;
             }
         }
-
         return ans;
     }
 };
