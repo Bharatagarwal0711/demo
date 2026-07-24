319af030779b093d1a78644a4f8bd80d02500017
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-25 03:36:08 +0530

    Time: 0 ms (100%), Space: 14.3 MB (29.76%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        
+        int n = prices.size();
+        stack<int> st;
+        vector<int> ans(n);
+
+        for(int i=n-1;i>=0;i--){
+            
+            while(!st.empty() && st.top() > prices[i]) st.pop();
+
+            ans[i] = prices[i] - ( st.empty() ? 0 : st.top());
+
+            st.push(prices[i]);
+        }
+
+        return ans;
+
+    }
+};
