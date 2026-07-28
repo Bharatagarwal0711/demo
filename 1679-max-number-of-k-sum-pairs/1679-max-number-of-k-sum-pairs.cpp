05d5b9daf9bc800611f314c5e3368823569372de
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-07-28 16:03:12 +0530

    Time: 120 ms (19.23%), Space: 71.9 MB (26.74%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        multiset<int> s;
+        unordered_map<int,int> s;
         int count = 0;
 
         for(int ele: nums){
@@ -9,9 +9,10 @@ public:
 
             if(s.find(diff) != s.end()){
                 count++;
-                s.erase(s.find(diff));
+                s[diff]--;
+                if(s[diff] == 0) s.erase(diff);
             }
-            else s.insert(ele);
+            else s[ele]++;
         }
 
         return count;
