d2b81749b8ca434d0482f85bcf7dc04995ee3048
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-01 18:14:22 +0530

    Time: 0 ms (100%), Space: 11.2 MB (20.77%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -1,64 +1,41 @@
 /**
-class ListNode {
-public:
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        if(head == NULL || head->next == NULL) return head;
-        
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        ListNode* nxt = head->next;
-
-        while(nxt){
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
-            nxt = nxt->next;
-        }
-        curr->next = prev;
-        return curr;
-    }
-
     ListNode* swapPairs(ListNode* head) {
         if(head == NULL || head->next == NULL) return head;
-        
-        ListNode Dummy(0);
+
+        ListNode Dummy;
+
         ListNode* temp = &Dummy;
-
         ListNode* slow = head;
-        ListNode* fast = head->next;
-        ListNode* nxt = fast->next;
-
 
         while(slow && slow->next){
-            fast->next = NULL;
 
-            temp->next = reverseList(slow);
-            slow->next = nxt;
-            temp = slow;
+
+            ListNode* nxt = slow->next->next;
+
+            temp->next = slow->next;
+            temp = temp->next;
+
+            ListNode* var = slow;
+            temp->next = var;
+
+            temp = temp->next;
 
             slow = nxt;
 
-            if(slow && slow->next){
-                fast = slow->next;
-                nxt = slow->next->next;
-            }
-            else break;
         }
+        temp->next = slow;
 
         return Dummy.next;
-
-
-
-        
-
     }
 };
