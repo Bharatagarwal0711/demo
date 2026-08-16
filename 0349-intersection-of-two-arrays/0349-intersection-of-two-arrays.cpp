11582660424666e30ab9b2d934b527c7f85f338f
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-16 08:38:55 +0530

    Time: 2 ms (43.01%), Space: 14.2 MB (68.04%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         set<int> s1(nums1.begin(),nums1.end());
-        set<int> s2(nums2.begin(),nums2.end());
-
         vector<int> ans;
 
-        for(int ele: s1){
-            if(s2.find(ele) != s2.end()) ans.push_back(ele);
+        for(int ele: nums2){
+            if(s1.find(ele) != s1.end()){
+                ans.push_back(ele);
+                s1.erase(ele);
+            }
         }
 
         return ans;
