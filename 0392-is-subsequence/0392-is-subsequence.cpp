8380c1f7bcb0e45cd49ca1056917055d21b2985f
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-20 00:02:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.55%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,13 +3,12 @@ public:
     bool isSubsequence(string s, string t) {
         int i = 0, j = 0;
 
-        while(i<s.size()){
-            if(j == t.size()) return false;
+        while(i<s.size() && j<t.size()){
 
             if(t[j] == s[i]) i++;
             j++;
 
         }
-        return true;
+        return i == s.size();
     }
 };
