0cc776509020e610021e2a0610cd6d242b7745e1
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-19 21:09:29 +0530

    Time: 0 ms (100%), Space: 8.8 MB (72.03%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        stringstream ss(s);
+        string word;
+        unordered_map<char,string> CtoS;
+        unordered_map<string,char> StoC;
+
+        int i = 0;
+
+
+        while(ss >> word){
+            if(i == pattern.size()) return false;
+
+            if(CtoS.find(pattern[i]) != CtoS.end() && CtoS[pattern[i]] != word  ) return false;
+
+            if(StoC.find(word) != StoC.end() && StoC[word] != pattern[i]  ) return false;
+
+
+            CtoS[pattern[i]] = word;
+            StoC[word] = pattern[i];
+
+            i++;
+            
+        }
+
+        return i==pattern.size();
+    }
+};
