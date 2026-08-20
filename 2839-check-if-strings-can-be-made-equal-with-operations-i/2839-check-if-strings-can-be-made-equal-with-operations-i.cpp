f8b0cc19af59aa24d92bf98834564657705da2bb
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-20 19:45:15 +0530

    Time: 0 ms (100%), Space: 8.4 MB (45.43%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        int i=0, j=0;
+
+
+        while(i<4){
+            if(s1[i] == s2[j]){
+                i++;
+                j++;
+            }
+            else{
+                if(i+2 < 4){
+                    swap(s2[i],s2[i+2]);
+
+                    if(s1[i] != s2[j]) return false;
+                    i++;
+                    j++;
+                }
+                else return false;
+            }
+        }
+
+        return true;
+
+        
+    }
+};
