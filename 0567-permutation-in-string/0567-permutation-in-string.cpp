4fd3bfda112820338786212a8f94ff1af02470a1
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-20 21:44:31 +0530

    Time: 111 ms (21.86%), Space: 21.1 MB (11.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if(s2.size()<s1.size()) return false;
+        
         vector<int> str1(26);
         for(char ch: s1){
             str1[ch-'a']++;
