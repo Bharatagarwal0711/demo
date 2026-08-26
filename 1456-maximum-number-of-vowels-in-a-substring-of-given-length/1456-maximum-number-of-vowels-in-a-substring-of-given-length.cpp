1738657723c2615ee1bcdd4cbe24c31592e50bcd
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-26 13:55:07 +0530

    Time: 6 ms (57.6%), Space: 13 MB (89.96%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maxVowels(string str, int k) {
-        unordered_set<char> vowels;
-        vowels.insert('a'); vowels.insert('e'); vowels.insert('i'); vowels.insert('o'); vowels.insert('u');
+        string vowels = "aeiou";
 
         int count = 0;
-        for(int i=0;i<k;i++) if(vowels.find(str[i]) != vowels.end()) count++;
+        for(int i=0;i<k;i++) if(vowels.find(str[i]) != string::npos) count++;
 
         int j = k;
         int ans = count;
         int n = str.size();
         for(int i=1;i<=n-k;i++){
 
-            if(vowels.find(str[i-1]) != vowels.end()) count--;
-            if(vowels.find(str[j]) != vowels.end()) count++;
+            if(vowels.find(str[i-1]) != string::npos) count--;
+            if(vowels.find(str[j]) != string::npos) count++;
             
             ans = max(ans,count);
             j++;
