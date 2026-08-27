14385e6a0a06429b69d2d05effeba7c0db2bb3d4
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-27 12:57:48 +0530

    Time: 0 ms (100%), Space: 8.3 MB (86.12%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string str1 = "";
+        string str2 = "";
+
+        for(char ch: s){
+
+            if(ch == '#' && str1.size() == 0) continue;
+            else if(ch == '#')  str1.pop_back();
+            else str1.push_back(ch);
+        }
+
+        for(char ch: t){
+
+            if(ch == '#' && str2.size() == 0) continue;
+            else if(ch == '#')  str2.pop_back();
+            else str2.push_back(ch);
+        }
+
+        return str1 == str2 ;
+
+
+    }
+};
