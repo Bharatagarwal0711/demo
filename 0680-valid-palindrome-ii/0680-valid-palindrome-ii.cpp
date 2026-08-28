e7fe8540636c7d4f47cbe5daca2603efa73d473f
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-08-28 17:06:13 +0530

    Time: 6 ms (34.14%), Space: 22.2 MB (41.28%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -3,9 +3,8 @@ public:
 
     bool check(string &s, int i, int j) {
         while(i < j) {
-            if(s[i] != s[j])
-                return false;
-
+            if(s[i] != s[j]) return false;
+            
             i++;
             j--;
         }
@@ -14,18 +13,16 @@ public:
     }
 
     bool validPalindrome(string s) {
+
         int i = 0;
         int j = s.size() - 1;
 
         while(i < j) {
 
-            if(s[i] == s[j]) {
-                i++;
-                j--;
-            }
-            else {
-                return check(s, i + 1, j) || check(s, i, j - 1);
-            }
+            if(s[i] != s[j])  return check(s, i + 1, j) || check(s, i, j - 1);
+            
+            i++;
+            j--;
         }
 
         return true;
