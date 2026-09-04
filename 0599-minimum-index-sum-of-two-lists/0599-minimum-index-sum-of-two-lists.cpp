5b78f3753c0d93a20513199903c373d5354e9431
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-09-04 16:34:43 +0530

    Time: 12 ms (87.82%), Space: 41.3 MB (46.86%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -11,32 +11,23 @@ public:
         int sum = 0;
         int mn = INT_MAX;
 
-        for(auto &str: list2){
-            if(mp.find(str) != mp.end()){
-                sum = i + mp[str];
-                if(sum < mn) mn = sum;
-            }
-
-            i++;
-        }
-
         vector<string> ans;
 
-        i = 0;
         for(auto &str: list2){
             if(mp.find(str) != mp.end()){
                 sum = i + mp[str];
-                if(sum == mn) ans.push_back(str);
+                if(sum < mn){
+                    mn = sum;
+                    ans.clear();
+                    ans.push_back(str);
+                }
+                else if(sum == mn) ans.push_back(str);
             }
 
             i++;
         }
 
         return ans;
-
-
-
-
 
     }
 };
