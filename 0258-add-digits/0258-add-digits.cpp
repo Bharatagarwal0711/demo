22619d72b1ff4d08a0036078e43e411313e8f3d3
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-09-05 12:28:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (45.35%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -3,10 +3,9 @@ public:
     int addDigits(int num) {
         if(num < 10) return num;
 
-        int sum = 10;
+        int sum = 0;
 
-        while(sum > 9){
-
+        do{
             sum = 0;
             
             while(num){
@@ -16,7 +15,7 @@ public:
 
             num = sum;
 
-        }
+        } while(sum > 9);
 
         return sum;
     }
