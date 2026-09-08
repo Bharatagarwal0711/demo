a5204123152ec34a0c6c70bb57777ccdf34eeaf0
Bharat Agarwal <bharatagarwal0711@gmail.com>
2026-09-08 12:56:03 +0530

    Time: 26 ms (48.51%), Space: 43.9 MB (55.19%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+
+        int ans = 0;
+        unordered_map<int,int> mp;
+
+        for(int ele: nums) mp[ele]++;
+
+        for(auto [key,value]: mp){
+            if(mp.find(key-1) != mp.end()){
+                ans = max(ans,value+mp[key-1]);
+            }
+            if(mp.find(key+1) != mp.end()){
+                ans = max(ans,value+mp[key+1]);
+            }
+        }
+
+        return ans;
+
+    }
+};
